47bbf8c07bf907730616ab6872df9a6c08a7f3a4
Joel Matthys <joel@matthysmusic.com>
2020-07-02 02:05:16 -0500

    first commit

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    [SerializeField] int timeToWait = 4;
+    int currentSceneIndex;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentSceneIndex =  SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex == 0)
+        {
+            // wait a few seconds
+            StartCoroutine(WaitForTime(timeToWait));
+        }
+    }
+
+    IEnumerator WaitForTime(int wait)
+    {
+        yield return new WaitForSeconds(wait);
+        LoadNextScene();
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void Quit() 
+    {
+        return;
+    }
+}
